feb02095b9ca30920366d5a9370e358e10851616
Doug Wollison <Doug@Dougs-iMac.local>
2014-03-04 16:05:31 -0500

    Added table setup script.

---
+++ wpmonitor/inc/setup.php
@@ -0,0 +1,34 @@
+<?php
+if(!$wmdb->ready){
+	die('Unable to connect to database');
+}
+
+// Create the users table
+$wmdb->query("CREATE TABLE `users` (
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`username` varchar(20) NOT NULL DEFAULT '',
+	`password` char(64) NOT NULL DEFAULT '',
+	`email` varchar(100) NOT NULL DEFAULT '',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `username` (`username`)
+)");
+
+// Create the sites table
+$wmdb->query("CREATE TABLE `sites` (
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`url` varchar(255) NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`version` varchar(9) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `url` (`url`)
+)");
+
+// Create the plugins table
+$wmdb->query("CREATE TABLE `plugins` (
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`site` bigint(20) unsigned NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`version` varchar(12) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `site_plugin_version` (`site`,`name`,`version`)
+);");
