4252a6db4af67922c9392f99f7bef5e73dda4b7b
Doug Wollison <doug@wollison.net>
2014-03-04 20:33:56 -0500

    Revised authentication script.

--- wpmonitor/inc/auth.php
+++ wpmonitor/inc/auth.php
@@ -1,15 +1,52 @@
 <?php
-if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
-    header('WWW-Authenticate: Basic realm="Premise: WP Monitor"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'Authorization Required';
-    exit;
-} else {
-	list($user, $pass) = explode(':' , base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
-	if($user != AUTH_USER || $pass != AUTH_PASS){
-		header('WWW-Authenticate: Basic realm="Premise: WP Monitor"');
-	    header('HTTP/1.0 401 Unauthorized');
-	    echo 'Access Denied';
-	    exit;
+/**
+ * The authentication script.
+ * You can replace this with your preferred system.
+ * Simply have it exit to the login form should it
+ * not authenticate properly.
+ */
+
+session_start();
+
+if(isset($_GET['logout'])){
+	// Destroy the auth token
+	unset($_SESSION['auth']);
+	
+	// Redirect back to referrer if possible
+	if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']){
+		header('Location: '.$_SERVER['HTTP_REFERER']);
+		exit;
 	}
+}
+
+$authenticated = false;
+
+// First, check if an auth session is present
+if(isset($_SESSION['auth'])){
+	// Validate auth session, show login form if failed
+	if(sha1(AUTH_USER.AUTH_PASS) == $_SESSION['auth']){
+		$authenticated = true;
+	}
+}
+// Alternately, check if credentials were posted
+elseif(isset($_POST['username']) && isset($_POST['password'])){
+	// Validate credentials, show login form if failed
+	if(AUTH_USER == $_POST['username'] && AUTH_PASS == $_POST['password']){
+		$authenticated = true;
+		
+		// Set the auth session
+		$_SESSION['auth'] = sha1(AUTH_USER.AUTH_PASS);
+		
+		// Redirect back to referrer if possible
+		if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']){
+			header('Location: '.$_SERVER['HTTP_REFERER']);
+			exit;
+		}
+	}
+}
+
+// If not authenticated, show login form and exit
+if(!$authenticated){
+	require('login.php');
+	exit;
 }
